0e2e9012acdbf988ba515837e8fff12a969f1f7e
Dibya <dibya.jyotiprakas@gmail.com>
2019-06-30 18:32:06 +0530

    webhook

--- src/test/java/com/hybridpro/test/VerifyLoginTest.java
+++ src/test/java/com/hybridpro/test/VerifyLoginTest.java
@@ -9,17 +9,18 @@ import com.aventstack.extentreports.Status;
 import com.hybridpro.initiate.Base;
 
 
-public class Test1001_VerifyLogin extends Base
+public class VerifyLoginTest extends Base
 {
 	
 	@Test
-	public void Test1(Method result)
+	public void LoginTest(Method result)
 	{
 		
 		GetUrl("admin");
 		logger.info("Application launched");
 		logger.info("Please start automation");
 		extentTest.log(Status.INFO, result.getName()+" started...");
+		System.out.println("************HYBRID PRO SELENIUM************************** ");
 	}
 
 }
